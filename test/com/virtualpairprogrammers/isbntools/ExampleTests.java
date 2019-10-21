d0d136d1ba5be6cbc3b930967a5a090d5367d9e9
yanyichi <yanyichi143@hotmail.com>
2019-10-21 06:19:54 -0700

    finish

---
+++ test/com/virtualpairprogrammers/isbntools/ExampleTests.java
@@ -0,0 +1,13 @@
+package com.virtualpairprogrammers.isbntools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleTests {
+
+    @Test
+    public void exampleTest(){
+        //fail();
+    }
+}
